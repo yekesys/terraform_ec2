35fea770e64302f177bd59b9fb0c1f05f3270b9d
cthullen-chwy <119418815+cthullen-chwy@users.noreply.github.com>
2023-02-23 09:50:31 -0500

    Update outputs.tf

--- a/aws_ec2_inst/outputs.tf
+++ a/aws_ec2_inst/outputs.tf
@@ -1,10 +1,15 @@
 
-output "public_ip" {
+output "instance_id" {
+  value = aws_instance.forpython.public_ip
+  description = "ID of the EC2 instance"
+}
+
+output "instance_public_ip" {
   value = aws_instance.forpython.public_ip
   description = "The public IP of the web server"
 }
 
-output "port" {
+output "instance_server_port" {
   value       = var.server_port
   description = "The port of the web server"
 }
